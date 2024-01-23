2a7127659cf62c95ebd23e188393ebf21f08d6d9
ijontan <85871000+ijontan@users.noreply.github.com>
2024-01-23 15:36:31 +0800

    some testing

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -3,5 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
 
+#include "webserv.hpp"
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 int main(int argc, char const *argv[])
 {
-    (void)argc;
-    (void)argv;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    struct addrinfo hints, *res;
+    int sockFd, newFd;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: showip hostname\n");
+        return 1;
+    }
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(NULL, argv[1], &hints, &res) != 0)
+    {
+        // fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(errno));
+        return 2;
+    }
+
+    sockFd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if (bind(sockFd, res->ai_addr, res->ai_addrlen))
+    {
+        printf("bind error\n");
+        return 3;
+    }
+
+    if (listen(sockFd, 10))
+    {
+        printf("listen error\n");
+        return 4;
+    }
+    addr_size = sizeof(their_addr);
+    newFd = accept(sockFd, (struct sockaddr *)&their_addr, &addr_size);
+
+    char buff[20];
+    while (recv(newFd, buff, 20, 0) > 0)
+    {
+        printf("server: got connection from %s\n", buff);
+        send(newFd, "Hello, world!", 13, 0);
+    }
+    freeaddrinfo(res); // free the linked list
+    (void)newFd;
     return 0;
 }
