ecd29d16ccad4b3559ab350b6e2f289469b5d050
Ng Wai Keat <nwai-kea@u90z08s01.42kl.edu.my>
2024-03-05 15:35:15 +0800

    cgi draft code

--- includes/Cgi.hpp
+++ includes/Cgi.hpp
@@ -2,20 +2,35 @@
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <map>
+#include <vector>
 
 class WebServer;
 
 class Cgi
 {
 private:
-	std::string receivedRaw;
+	struct rInfo
+	{
+		std::vector<std::string> request;
+		std::map<std::string, std::string> headers;
+		std::string body;
+	};
+	std::string path;
+	std::map<std::string, std::string> envVariables;
+	char **envV;
+	void setEnv(Cgi::rInfo &rqi);
+	void tokenize(std::string s, Cgi::rInfo &rsi) const;
 
 public:
 	Cgi();
+	Cgi(std::string path);
 	~Cgi();
 	Cgi(const Cgi &src);
 	Cgi &operator=(const Cgi &rhs);
-	std::string getRaw() const;
-	virtual void recieveMessage(std::string raw);
-	virtual std::string getMessageToSend(WebServer &ws);
+	
+	virtual std::string runCgi(IOAdaptor &io, const std::string path);
+	std::string getPath() const;
+	void setPath(const std::string path);
 };
--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -1,8 +1,15 @@
 #include "Cgi.hpp"
 #include "colors.h"
+#include "IOAdaptor.hpp"
+#include "utils.hpp"
 #include <sstream>
+#include <stdlib>
 
-Cgi::Cgi(){}
+Cgi::Cgi(): path("")
+{}
+
+Cgi::Cgi(std::string path) : path(path)
+{}
 
 Cgi::~Cgi(){}
 
@@ -11,26 +18,71 @@ Cgi::Cgi(const Cgi &src)
 	*this = src; 
 }
 
+void Cgi::setEnv(Cgi::rInfo &rqi)
+{
+	this->envVariables["SCRIPT_NAME"] = rqi.request[1];
+	this->envVariables["REQUEST_METHOD"] = rqi.request[0];
+	this->envV = (char **)calloc(sizeof(char *), this->envVariables.size() + 1);
+	std::map<std::string, std::string>::const_iterator it = this->envVariables.begin();
+	for (int i = 0; it != this->envVariables.end(); it++; i++)
+	{
+		std::string tmp = it->first + "=" it->second;
+		this->envV[i] = strdup(tmp.c_str());
+	}
+}
+
 Cgi &Cgi::operator=(const Cgi &rhs)
 {
 	return *this;
 }
 
-std::string Cgi::getRaw() const
+std::string Cgi::runCgi(IOAdaptor &io, const std::string path)
 {
-	return receivedRaw;
+	pid_t pid;
+	int input[2];
+	int output[2];
+	Cgi::rInfo rqi;
+
+	tokenize(io.getRaw(), rqi);
+	// setPath(path);
+	std::string dir = getPath().substr(0, getPath().find_first_of("/", 2) + 1);
+
+	char *av[3] = {(char *)this->path.c_str(), (char *)dir.c_str, NULL};
+
+	if (pipe(input) == -1 || pipe(output) == -1)
+		return ("fail\n");
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(input[0], STDIN_FILENO);
+		dup2(output[1], STDOUT_FILENO);
+		close(input[0]);
+		close(input[1]);
+		close(output[0]);
+		close(output[1]);
+		int i = execve(this->path.c_str(), av, this->envV)
+		if (i < 0)
+			exit(i);
+	}
 }
 
-void Cgi::recieveMessage(std::string raw)
-{
-	receivedRaw = raw;
-}
+// std::string Cgi::getPath() const
+// {
+// 	return this->path;
+// }
 
-std::string Cgi::getMessageToSend(WebServer &ws)
+// void Cgi::setPath(const std::string path)
+// {
+// 	this->path = path;
+// }
+
+void Cgi::tokenize(std::string s, Cgi::rInfo &rsi) const
 {
-	(void)ws;
-	std::stringstream ss;
-	ss << "Recieved message:\n"
-	   << receivedRaw << "\nSending back: Hello, world!\n";
-	return ss.str();
+	std::pair<std::string, std::string> headerBody = utils::splitPair(s, "\r\n\n");
+	std::vector<std::string> requestHeader = utils::split(headerBody.first, "\r\n");
+
+	rsi.request = utils::split(requestHeader[0], ' ');
+	for (size_t i = 1; i < requestHeader.size(); i++)
+		rsi.headers.insert(utils::splitPair(requestHeader[i], ": "));
+	rsi.body = headerBody.second;
 }
